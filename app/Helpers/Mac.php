aaf97bf35f7778d102864c699c1174033ac83e4d
Ben Stapleton <webdev.ben@gmail.com>
2025-04-24 08:10:38 +0100

    Convert to uppercase before searching

--- app/Helpers/Mac.php
+++ app/Helpers/Mac.php
@@ -8,6 +8,7 @@ class Mac
     {
         $value = str_replace(['-', ':', '.', ' '], '', $mac);
         $value = str_split($value, 6);
+        $value = strtoupper($value[0]);
 
         return $value[0];
     }
